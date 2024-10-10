6c3904aa839e5630b0f82724beca3c5d0c32b27b
PhilipDet <philipdethlefsen251@gmail.com>
2024-10-10 12:54:03 +0200

    SHake animation "shaki shaki"

--- ASSETS/JS/modules/menu/menu.js
+++ ASSETS/JS/modules/menu/menu.js
@@ -1,20 +1,41 @@
 export default function menu(data) {
+    let currentDay = new Date().getDay();
     const menuElement = document.getElementById("menu-container");
     const h2Element = document.querySelector("#menu hgroup h2");
     h2Element.innerHTML = `DEN VARME <br> UGE ${data.Week}`;
     let html = "";
 
     data.Days.forEach((day) => {
-        let dayShortName =
-            day.DayName === "torsdag" ? "tors" : day.DayName.slice(0, 3);
+        let dayShortName = day.DayName.slice(0, 3);
 
-        html += `
+        console.log(day.DayName, daysName[currentDay]);
+
+        if (day.DayName === daysName[currentDay]) {
+            html += `
+            <div class="today">
+                <h3>${dayShortName}</h3>
+                <h3>${day.Dish.slice(0, -12)}</h3>
+            </div>
+        `;
+        } else {
+            html += `
             <div>
                 <h3>${dayShortName}</h3>
                 <h3>${day.Dish.slice(0, -12)}</h3>
             </div>
         `;
+        }
     });
 
     menuElement.innerHTML += html;
 }
+
+let daysName = [
+    "søndag",
+    "mandag",
+    "tirsdag",
+    "onsdag",
+    "torsdag",
+    "fredag",
+    "lørdag",
+];
